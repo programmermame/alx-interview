0a92bb1e89f54f57b3139fa6f0eec9fd86e5f5aa
programmermame <mameatpickout@gmail.com>
2024-06-13 23:44:27 +0300

    minimum-operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""
+Minimum Operations
+Given num n, write a method that calculates the fewest number of operations
+needed to result in exactly n H characters in a file
+Prototype: def minOperations(n)
+Returns an integer
+if n is impossible to achieve, return 0
+"""
+
 
 def minOperations(n):
+    """
+    Function minOperations
+    Returns an integer
+    """
     result = 0
     x = 2
     while n > 1:
